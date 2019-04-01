c22a5eb2cca9b86456ae9e6aa377702a9538d115
littlepretty@XcodeMac <littlepretty881203@gmail.com>
2019-03-31 21:38:29 -0500

    Finish assignment3 with not good result
    Signed-off-by: littlepretty@XcodeMac <littlepretty881203@gmail.com>

--- a3/a3.py
+++ a3/a3.py
@@ -86,20 +86,49 @@ def featurize(movies):
       - The movies DataFrame, which has been modified to include a column named 'features'.
       - The vocab, a dict from term to int. Make sure the vocab is sorted alphabetically as in a2 (e.g., {'aardvark': 0, 'boy': 1, ...})
 
-    >>> movies = pd.DataFrame([[123, 'Horror|Romance'], [456, 'Sci-Fi']], columns=['movieId', 'genres'])
-    >>> featurize(movies)
+    >>> movies = pd.DataFrame([[123, 'Horror|Romance'], [456, 'Sci-Fi|Horror']], columns=['movieId', 'genres'])
+    >>> movies, vocab = featurize(movies)
+    >>> vocab
+    {'horror': 0, 'romance': 1, 'sci-fi': 2}
+    >>> movies['features'][0].toarray()
+    array([[0.     , 0.30103, 0.     ]])
+    >>> movies['features'][1].toarray()
+    array([[0.     , 0.     , 0.30103]])
     """
-    movies = tokenize(movies)
     termSet = set()
     for index, row in movies.iterrows():
         for term in list(row.tokens):
             termSet.add(term)
 
     vocab = {word: index for index, word in enumerate(sorted(list(termSet)))}
-    tf = defaultdict(lambda: defaultdict(lambda: 0)) # term i + doc d -> freq of i in d
-    df = defaultdict(lambda: 0) # term i -> #docs that contains term i
-    maxtf = defaultdict(lambda: 0) # doc d -> max_k{tf[k][d]}
+    tf = defaultdict(lambda: defaultdict(lambda: 0.0)) # term i + doc d -> freq of i in d
+    df = defaultdict(lambda: 0.0) # term i -> #docs that contains term i
+    maxtf = defaultdict(lambda: 0.0) # doc d -> max_k{tf[k][d]}
 
+    for d, movie in movies.iterrows():
+        for term in list(movie.tokens):
+            tf[vocab[term]][d] += 1.0
+            maxtf[d] = max(maxtf[d], tf[vocab[term]][d])
+
+        for term in list(set(movie.tokens)):
+            df[vocab[term]] += 1.0
+
+    columnVals = []
+    for d, movie in movies.iterrows():
+        values, colIndices = [], []
+        for term in list(set(movie.tokens)):
+            i = vocab[term]
+            tfidf = tf[i][d] / maxtf[d] * math.log10(movies.shape[0] / df[i])
+            values.append(tfidf)
+            colIndices.append(i)
+
+        csrFeatures = csr_matrix((values, ([0] * len(values), colIndices)),
+                                 shape=(1, len(vocab)))
+        columnVals.append(csrFeatures)
+
+    featurePd = pd.DataFrame({'features': columnVals})
+    movies = movies.join(featurePd)
+    return movies, vocab
 
 
 def train_test_split(ratings):
@@ -122,9 +151,15 @@ def cosine_sim(a, b):
     Returns:
       A float. The cosine similarity, defined as: dot(a, b) / ||a|| * ||b||
       where ||a|| indicates the Euclidean norm (aka L2 norm) of vector a.
+    >>> a = csr_matrix([0., 0., 3., 0., 4.], shape=(1, 5))
+    >>> b = csr_matrix([0., 0., 3., 4., 0.], shape=(1, 5))
+    >>> cosine_sim(a, b)
+    0.36
     """
-    ###TODO
-    pass
+    def norm(a):
+        return np.sqrt(a.multiply(a).sum())
+
+    return a.dot(b.transpose()).sum() / (norm(a) * norm(b))
 
 
 def make_predictions(movies, ratings_train, ratings_test):
@@ -149,8 +184,30 @@ def make_predictions(movies, ratings_train, ratings_test):
     Returns:
       A numpy array containing one predicted rating for each element of ratings_test.
     """
-    ###TODO
-    pass
+    predictions = []
+    train_movies = ratings_train.join(movies, on='movieId', how='inner',
+                                      lsuffix='_rating', rsuffix='_movie')
+    for _, row in ratings_test.iterrows():
+        uid, mid = int(row.userId), int(row.movieId)
+        unratedMovie = movies[movies.movieId == mid]
+        ratedMovies = train_movies[train_movies.userId == uid]
+        rating, totalSim = 0.0, 0.0
+        for _, movie in ratedMovies.iterrows():
+            sim = cosine_sim(movie.features, list(unratedMovie.features)[0])
+            if sim > 0:
+                rating += sim * movie.rating
+                totalSim += sim
+
+        if totalSim == 0.0:
+            rating = np.mean(ratedMovies.rating)
+            print('Fallback to average rating = %.2f' % rating)
+        else:
+            rating /= totalSim
+            # print('Weighted over similarity rating = %.2f' % rating)
+
+        predictions.append(rating)
+
+    return np.array(predictions)
 
 
 def mean_absolute_error(predictions, ratings_test):
@@ -167,13 +224,16 @@ def main():
     movies = pd.read_csv(path + os.path.sep + 'movies.csv')
     movies = tokenize(movies)
     movies, vocab = featurize(movies)
-    print('vocab:')
+    print(movies[:5])
+    print(ratings[:5])
+    print('vocab (size=%d):' % len(vocab))
     print(sorted(vocab.items())[:10])
     ratings_train, ratings_test = train_test_split(ratings)
     print('%d training ratings; %d testing ratings' % (len(ratings_train), len(ratings_test)))
     predictions = make_predictions(movies, ratings_train, ratings_test)
     print('error=%f' % mean_absolute_error(predictions, ratings_test))
     print(predictions[:10])
+    print(ratings_test.rating[:10])
 
 
 if __name__ == '__main__':
